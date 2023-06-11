dfba62a27c465ab263351a3e2d28e5d0787938f2
As7Vezes <denysn60@gmail.com>
2023-06-11 13:20:12 -0300

    updating-code

--- Projeto_Django/urls.py
+++ Projeto_Django/urls.py
@@ -1,14 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls')),
+    path('', include('app.urls')),
     path('auth/', include('app.authUrls'))
 ]
